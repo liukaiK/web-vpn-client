ce5eed70756d8ab767d97e677f2b5b45893d77f5
liukai <zhizhufan@foxmail.com>
2022-12-10 21:13:21 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,1 @@
+# web-vpn-client
---
+++ src/main/java/cn/com/goodlan/webvpn/ClientApplication.java
@@ -0,0 +1,22 @@
+package cn.com.goodlan.webvpn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableAsync
+@EnableJpaAuditing
+@EnableScheduling
+@EnableWebSecurity(debug = false)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+}
